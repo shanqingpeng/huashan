a77ea70c891fcd65f819f76e7185f6b8cff7ac45
shanqingpeng <shanqingpeng@sqp.com>
2019-02-12 16:27:50 +0800

    push script

---
+++ push_to_remote.sh
@@ -0,0 +1,15 @@
+git add microservicecloud-eureka-server-7001
+git add microservicecloud-eureka-server-7002
+git add microservicecloud-eureka-server-7003
+git add microservicecloud-provider-dept-8001
+git add microservicecloud-service-consumer-8002
+git microservicecloud-service-provider-8001
+
+git commit -m "my practice" microservicecloud-api
+git commit -m "my practice" microservicecloud-consumer-dept-80
+git commit -m "my practice" microservicecloud-eureka-server-7001
+git commit -m "my practice" microservicecloud-eureka-server-7002
+git commit -m "my practice" microservicecloud-eureka-server-7003
+git commit -m "my practice" microservicecloud-provider-dept-8001
+git commit -m "my practice" microservicecloud-service-consumer-8002
+git commit -m "my practice" microservicecloud-service-provider-8001
